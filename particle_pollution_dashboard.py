2ddf3d864cb93f67b872a7f608f717a8c218ed49
Marta Dujella <marta.dujella@gmail.com>
2026-03-20 15:24:25 +0100

    Add Dockerfile and requirements for Hugging Face deployment

--- particle_pollution_dashboard.py
+++ particle_pollution_dashboard.py
@@ -369,4 +369,6 @@ def update_covid_plots(selected_pollutants):
 
     return plots
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=False)
+
